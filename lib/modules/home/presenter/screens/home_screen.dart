6b91e17f39113cf624261e17cebe6dcffeac6f12
João Vitor <jovioli.dev04@gmail.com>
2023-05-13 01:03:36 -0300

    feat: finalizes the implementation of the home screen layout

--- lib/modules/home/presenter/screens/home_screen.dart
+++ lib/modules/home/presenter/screens/home_screen.dart
@@ -7,7 +7,15 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
+  late final TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,8 +40,13 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             const SizedBox(height: 32),
             TextField(
+              cursorColor: Colors.white,
               style: Theme.of(context).textTheme.bodySmall,
               decoration: InputDecoration(
+                suffixIcon: const Icon(
+                  Icons.arrow_circle_right_outlined,
+                  color: Colors.white,
+                ),
                 labelText: 'Futebol, filmes, séries...',
                 labelStyle: Theme.of(context)
                     .textTheme
@@ -42,7 +55,87 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
             const SizedBox(height: 100),
-            // SegmentedButton(segments: segments, selected: selected)
+            TabBar(
+              controller: _tabController,
+              indicatorColor: const Color(0xFF313040),
+              splashBorderRadius: const BorderRadius.all(Radius.circular(8)),
+              tabs: [
+                Tab(
+                  child: Text(
+                    'Recentes',
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                ),
+                Tab(
+                  child: Text(
+                    'Favoritos',
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 24),
+            Expanded(
+              child: Material(
+                color: Colors.transparent,
+                child: TabBarView(controller: _tabController, children: [
+                  ListView.builder(
+                    itemCount: 10,
+                    itemBuilder: (context, index) => Column(
+                      children: [
+                        ListTile(
+                          contentPadding: const EdgeInsets.all(16),
+                          tileColor: const Color(0xFF313040),
+                          dense: false,
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(8),
+                            ),
+                            side: BorderSide(color: Colors.white),
+                          ),
+                          title: Text(
+                            'Harry Potter',
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                          subtitle: Text(
+                            'Dificuldade: Média',
+                            style: Theme.of(context).textTheme.bodySmall,
+                          ),
+                        ),
+                        const Divider(),
+                      ],
+                    ),
+                  ),
+                  ListView.builder(
+                    itemCount: 10,
+                    itemBuilder: (context, index) => Column(
+                      children: [
+                        ListTile(
+                          contentPadding: const EdgeInsets.all(16),
+                          tileColor: const Color(0xFF313040),
+                          dense: false,
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(8),
+                            ),
+                            side: BorderSide(color: Colors.white),
+                          ),
+                          title: Text(
+                            'Avatar',
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                          subtitle: Text(
+                            'Dificuldade: Fácil',
+                            style: Theme.of(context).textTheme.bodySmall,
+                          ),
+                        ),
+                        const Divider(),
+                      ],
+                    ),
+                  ),
+                ]),
+              ),
+            )
           ],
         ),
       ),
